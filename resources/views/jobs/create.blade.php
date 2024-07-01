6ea58a79c75667d65ee2079040ebdffd07db50b8
Samm Caagbay <sammcaag@gmail.com>
2024-07-01 22:28:03 +0800

    👍 Done on validation with creating jobs

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,8 +17,12 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Jolibee Crew">
+                                    placeholder="Jolibee Crew"
+                                    required>
                             </div>
+                            @error('title')
+                                <p class="text-red-500 italic mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -31,8 +35,12 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$50,000 Per Year">
+                                    placeholder="$50,000 Per Year"
+                                    required>
                             </div>
+                            @error('salary')
+                                <p class="text-red-500 italic mt-1">{{ $message }}</p>
+                            @enderror
                         </div>
                     </div>
                 </div>
